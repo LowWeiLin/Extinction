377f01e577ac4038fb05adebfe525bea1ddb01d0
Low WeiLin <lwl1991@gmail.com>
2016-01-30 11:52:34 +0800

    Some skeleton code

---
+++ base.py
@@ -0,0 +1,23 @@
+
+
+
+class Base:
+
+    # Map
+    _map = None
+
+    def initialize():
+        pass
+
+    # Returns list of minions from own team
+    def findOwnMinions(self):
+        pass
+
+    # Returns list of minions from enemy teams
+    def findEnemyMinions(self):
+        pass
+
+    # Returns list of food
+    def findFood(self):
+        pass
+
---
+++ map.py
@@ -0,0 +1,37 @@
+
+
+class Map:
+    size = (100, 100)
+    foodNum = 30
+
+
+    grid = None
+    minionList = []
+    foodList = []
+
+    def initialize(self):
+        self.initializeEmptyMap()
+
+    def initializeEmptyMap(self):
+        self.grid = [[None] * self.size[0]] * self.size[1]
+
+    def populateFoodRandomly(self):
+        pass
+
+    def addRandomFood(self):
+        pass
+
+    def addMinion(self):
+        pass
+
+    def getMinionsTeam(self, team):
+        pass
+
+    def getMinionsExclTeam(self, team):
+        pass
+        
+'''
+m =  Map()
+m.initialize()
+print m.grid
+'''
